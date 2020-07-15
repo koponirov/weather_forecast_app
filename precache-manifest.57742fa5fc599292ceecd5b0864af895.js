91bdc7ed8b82a079724875402533075378e88e2f
m.koponirov <koponirov92@gmail.com>
2020-07-15 23:17:08 +0300

    Updates

---
+++ precache-manifest.57742fa5fc599292ceecd5b0864af895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d200390db372aea79898896f2d467ff",
+    "url": "/weather_forecast_app/index.html"
+  },
+  {
+    "revision": "be86d5cec6392a01c681",
+    "url": "/weather_forecast_app/static/css/main.16fd8507.chunk.css"
+  },
+  {
+    "revision": "8c84150e45a50592483a",
+    "url": "/weather_forecast_app/static/js/2.ab486316.chunk.js"
+  },
+  {
+    "revision": "eac0e0284d5461e5b08e9c3a7208f4cc",
+    "url": "/weather_forecast_app/static/js/2.ab486316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be86d5cec6392a01c681",
+    "url": "/weather_forecast_app/static/js/main.a3ad8f58.chunk.js"
+  },
+  {
+    "revision": "07f0eaf7ac2c27ec3a80",
+    "url": "/weather_forecast_app/static/js/runtime-main.0c65af04.js"
+  }
+]);
